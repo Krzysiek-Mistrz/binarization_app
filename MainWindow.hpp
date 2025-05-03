f779bab37bc5c0ae4dd2ae2815ccaf2fd8b1849f
Krzysztof <krzyskuar@gmail.com>
2025-05-03 23:51:26 +0200

    added some basic functionalities

---
+++ MainWindow.hpp
@@ -0,0 +1,40 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QLabel>
+#include <QSlider>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QFileDialog>
+#include <QImage>
+#include <QPixmap>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <vector>
+
+namespace Ui {
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+private slots:
+    void loadImage();
+    void updateManualThresholds();
+    void computeAutoThresholds();
+private:
+    cv::Mat src, processed;
+    QLabel *imageLabel;
+    QSlider *sliderR, *sliderG, *sliderB;
+    QPushButton *btnManual, *btnAuto;
+    void displayMat(const cv::Mat &mat);
+};
+
+#endif
